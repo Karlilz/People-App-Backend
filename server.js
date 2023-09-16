99e5f198d0b838db3c624dd7c2ff2e54bd490cbc
Karla Lazarte <karlalazarte@stephens-mbp.myfiosgateway.com>
2023-09-16 12:56:12 -0400

    adding in cors and express.json

--- server.js
+++ server.js
@@ -1,42 +1,21 @@
-// require('dotenv').config();
-// const express = require('express');
-// const app = express();
-// const PORT = process.env.PORT || 3000;
-// const peopleController = require('./controllers/peopleController');
-// const morgan = require('morgan');
-
-
-// app.use(morgan('tiny'))
-// // req.body will always be empty if we dont include express.urlencoded
-// app.use(express.urlencoded({extended: true}));
-
-// app.get('/', (req,res) => {
-//   res.send("HELLO WORLD")
-// })
-
-// // attach word people to routes 
-// app.use('/people', peopleController);
-
-// app.listen(PORT, () => {
-//   console.log("HERE GOES NOTHING!");
-// });
-
-
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
 const PORT = process.env.PORT || 3000;
-require('dotenv').config()
-const peopleController = require('./controllers/peopleController')
-const morgan = require('morgan')
+require('dotenv').config();
+const peopleController = require('./controllers/peopleController');
+const morgan = require('morgan');
+const cors= require ('cors');
 
-console.log("KARLA")
-
-app.use(express.urlencoded({ extended: true }))
-app.use(morgan('tiny'))
-app.use('/people', peopleController)
+app.use(cors());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(morgan('tiny'));
+app.use('/people', peopleController);
 
 app.get('/', (req, res) => {
     res.send("Hello world!")
 })
 
 app.listen(PORT,() => console.log(`Here were are on ${PORT}`))
+
+// when we want to use JSON, we need to tell our app to use JSON 
